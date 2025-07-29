3b25dbddf061e1afd016e476da7f88e73c493c9a
Mahboob-A <iammahboob.a@gmail.com>
2025-07-29 22:21:11 +0530

    chore: settings/local.py configuration for local development

--- finance_backend/settings/local.py
+++ finance_backend/settings/local.py
@@ -1,10 +1,44 @@
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-lj720@ps&b58^@t9s6x6-b3v_v&mbuv0tx8o0jmw2fnpi$$&tt'
+from os import getenv, path 
+from dotenv import load_dotenv
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+from settings.base import *  # noqa: F403, F401
+from settings.base import BASE_DIR
 
-ALLOWED_HOSTS = []
+local_env_file = path.join(BASE_DIR, '.envs', 'env.local')
+
+if path.isfile(local_env_file):
+    load_dotenv(local_env_file)
+
+
+##################### Secret Key and Host Configuration #####################
+SECRET_KEY = getenv('SECRET_KEY')
+
+DEBUG = getenv('DEBUG', 'False').lower() in ('true', '1', 'yes')
+
+SITE_NAME = getenv('SITE_NAME', 'Finance Backend')
+
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "0.0.0.0"]
+
+ADMIN_URL = getenv('ADMIN_URL', 'admin')
+
+##################### Email Configuration #####################
+EMAIL_BACKEND = "djcelery_email.backends.CeleryEmailBackend"
+EMAIL_HOST = getenv('EMAIL_HOST', 'localhost')
+EMAIL_PORT = int(getenv('EMAIL_PORT', 1025))
+DEFAULT_FROM_EMAIL = getenv('DEFAULT_FROM_EMAIL', 'webmaster@localhost')
+
+EMAIL_USE_TLS = getenv('EMAIL_USE_TLS', 'False').lower() in ('true', '1', 'yes')
+EMAIL_HOST_USER = getenv('EMAIL_HOST_USER', '')
+EMAIL_HOST_PASSWORD = getenv('EMAIL_HOST_PASSWORD', '')
+
+
+##################### Domain Configuration #####################
+DOMAIN = getenv('DOMAIN', 'localhost')
+if not DOMAIN.startswith('http'):
+    DOMAIN = f'http://{DOMAIN}' 
+    if not DOMAIN.endswith('/'):
+        DOMAIN += '/'   
+
+##################### Image Upload Configuration #####################
+MAXIMUM_UPLOAD_SIZE = 1 * 1024 * 1024  # 1 MB
